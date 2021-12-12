e6172a4810504183d19a283d989b8d6dd325d3b6
NP TECHNOLOGY <75479778+prakash766@users.noreply.github.com>
2021-12-12 20:31:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,13 @@ bot = Client('pdiskshortner bot',
              workers=50,
              sleep_threshold=10)
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Hi I am one and Only a personal Bot to short links from droplink website  Made with ♥️ by @NP_technology')
 
-
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, 'hey, bro I can convert big/long link to a short link of droplink  Made with ♥️ by @NP_technology')
 
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
 async def link_handler(bot, message):
